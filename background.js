2bda539afe18c68246a179a478f62f1c811f1262
Hudson Akridge <Hudson.Akridge@coxautoinc.com>
2019-06-12 17:41:23 -0500

    Added searching bookmark by the URL of a page load

--- background.js
+++ background.js
@@ -1,8 +1,19 @@
 chrome.webNavigation.onCompleted.addListener(function(details){
     if(details.frameId === 0) {
-        console.log(details.url);
+        //console.log(details.url);
+        // chrome.bookmarks.getTree(function(tree){
+        //     console.log(tree);
+        // });
+        chrome.bookmarks.search({url:details.url}, function(results){
+            if(results === undefined || results.length == 0){
+                console.log("No URL hits.");
+                return;
+            }
+            console.log("We got a hit.");
+
+            results.forEach(function(element){
+                console.log(`Bookmark Hit for - ${element.title}:${element.url}`);
+            });
+        });
     }
-    // chrome.tabs.get(details.tabId, function(tab) {
-    //     if(tab.url === details.url) {
-    //     }}
   });
