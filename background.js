0669f3fb53e85a0614a98f63ff88397c04f66fe3
Akridge <Hudson.Akridge@coxautoinc.com>
2019-08-07 09:48:50 -0500

    Implements Issue#2. Added a way to set/save the storage for objects and increment each hit.

--- background.js
+++ background.js
@@ -1,19 +1,52 @@
-chrome.webNavigation.onCompleted.addListener(function(details){
-    if(details.frameId === 0) {
-        //console.log(details.url);
-        // chrome.bookmarks.getTree(function(tree){
-        //     console.log(tree);
-        // });
-        chrome.bookmarks.search({url:details.url}, function(results){
-            if(results === undefined || results.length == 0){
-                console.log("No URL hits.");
-                return;
-            }
-            console.log("We got a hit.");
+String.prototype.hashCode = function() {
+  var hash = 0,
+    i,
+    chr;
+  if (this.length === 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    chr = this.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
 
-            results.forEach(function(element){
-                console.log(`Bookmark Hit for - ${element.title}:${element.url}`);
-            });
+chrome.webNavigation.onCompleted.addListener(function(details) {
+  if (details.frameId === 0) {
+    //console.log(details.url);
+    // chrome.bookmarks.getTree(function(tree){
+    //     console.log(tree);
+    // });
+    chrome.bookmarks.search({ url: details.url }, function(results) {
+      if (results === undefined || results.length == 0) {
+        console.log("No URL hits.");
+        return;
+      }
+
+      results.forEach(function(element) {
+        const bmrEntryKey = `bmr-${element.url.hashCode()}`;
+        chrome.storage.sync.get([`${bmrEntryKey}`], items => {
+          console.log(items);
+          let numberOfHits = 0;
+          //Get existing value if exists
+          if (items[bmrEntryKey]) {
+            const existingValue = items[`${bmrEntryKey}`];
+            console.log(`Existing Value: ${existingValue}`);
+            numberOfHits = existingValue;
+          }
+
+          //Set into storage
+          numberOfHits++;
+          let tracker = { [bmrEntryKey]: numberOfHits };
+          console.log(tracker);
+          chrome.storage.sync.set(tracker);
+          console.log(
+            `Bookmark Hit and stored for - ${element.title}:${
+              element.url
+            }. Hash: ${element.url.hashCode()}. Total hits: ${numberOfHits}`
+          );
         });
-    }
-  });
+      });
+    });
+  }
+});
