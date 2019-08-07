39f839f3613fdfb11ce64ceb9d70d1c3550ab390
Akridge <Hudson.Akridge@coxautoinc.com>
2019-08-07 13:17:32 -0500

    Some minor cleanup and adding the weight and bmr key to the bookmark entries

--- background.js
+++ background.js
@@ -11,47 +11,89 @@ String.prototype.hashCode = function() {
   return hash;
 };
 
+const keyPrefix = "bmr_";
+const convertUrlToKey = url => {
+  if (!url) {
+    return null;
+  }
+
+  return `${keyPrefix}${url.hashCode()}`;
+};
+
+const incrementStoredBookmarkEntry = async url => {
+  const bmrEntryKey = `${convertUrlToKey(url)}`;
+  let items = await chrome.storage.sync.get([`${bmrEntryKey}`]);
+
+  let numberOfHits = 0;
+  //Get existing value if exists
+  if (items[bmrEntryKey]) {
+    const existingValue = items[`${bmrEntryKey}`];
+    console.log(`Existing Value: ${existingValue}`);
+    numberOfHits = existingValue;
+  }
+
+  //Set into storage
+  numberOfHits++;
+  let tracker = { [bmrEntryKey]: numberOfHits };
+  await chrome.storage.sync.set(tracker);
+};
+
 const onNavigationCompleted = async details => {
-    if (details.frameId === 0) {
-        //console.log(details.url);
-        // chrome.bookmarks.getTree(function(tree){
-        //     console.log(tree);
-        // });
-        let results = await chrome.bookmarks.search({ url: details.url });
-        if (results === undefined || results.length == 0) {
-            console.log("No URL hits.");
-            return;
-          }
-    
-          results.forEach(async function(bookmarkEntry) {
-            const bmrEntryKey = `bmr-${bookmarkEntry.url.hashCode()}`;
-            let items = await chrome.storage.sync.get([`${bmrEntryKey}`]);
-            
-            let numberOfHits = 0;
-            //Get existing value if exists
-            if (items[bmrEntryKey]) {
-              const existingValue = items[`${bmrEntryKey}`];
-              console.log(`Existing Value: ${existingValue}`);
-              numberOfHits = existingValue;
-            }
-  
-            //Set into storage
-            numberOfHits++;
-            let tracker = { [bmrEntryKey]: numberOfHits };
-            await chrome.storage.sync.set(tracker);
-            console.log(
-              `Bookmark Hit and stored for - ${bookmarkEntry.title}:${
-                bookmarkEntry.url
-              }. Hash: ${bookmarkEntry.url.hashCode()}. Total hits: ${numberOfHits}`
-            );
-  
-            //Get Parent
-            //Get siblings
-            let siblingResults = await chrome.bookmarks.getChildren(bookmarkEntry.parentId);
-            console.log(siblingResults);
-            //Re-Order self and siblings in folder
-          });
-      }
-}
+  if (details.frameId !== 0) {
+    return;
+  }
+
+  //console.log(details.url);
+  // chrome.bookmarks.getTree(function(tree){
+  //     console.log(tree);
+  // });
+  let results = await chrome.bookmarks.search({ url: details.url });
+  if (results === undefined || results.length == 0) {
+    console.log("No URL hits.");
+    return;
+  }
+
+  results.forEach(async function(bookmarkEntry) {
+    await incrementStoredBookmarkEntry(bookmarkEntry.url);
+
+    console.log(
+      `Bookmark Hit and stored for - ${bookmarkEntry.title}:${
+        bookmarkEntry.url
+      }. Hash: ${bookmarkEntry.url.hashCode()}.`
+    );
+
+    //Get siblings
+    let siblingResults = await chrome.bookmarks.getChildren(bookmarkEntry.parentId);
+    //Add Bookmark Ranks to each sibling found
+    siblingResults.forEach(async sibling => {
+      let siblingKeyQuery = `${convertUrlToKey(sibling.url)}`;
+      sibling.bmrKey = siblingKeyQuery;
+
+      let siblingBmrEntry = await chrome.storage.sync.get(siblingKeyQuery);
+      let entryWeight = siblingBmrEntry[convertUrlToKey(sibling.url)] || 1;
+      sibling.weight = entryWeight;
+    });
+    console.log(siblingResults);
+
+    // let siblingKeyQuery = siblingResults.map(x => `${convertUrlToKey(x.url)}`);
+    // let siblingBmrEntries = await chrome.storage.sync.get(siblingKeyQuery);
+    // console.log(siblingBmrEntries);
+    // //go through each one and see if it's a url or a folder
+    // siblingResults.forEach(sibling => {
+    //   //if folder, skip
+    //   if (!sibling.url) {
+    //     return;
+    //   }
+    //   //if URL, check its weight value in the storage, defaulting to 1 if not found (had to hit it once to set the bookmark...right?)
+    //   let entryWeight = siblingBmrEntries[convertUrlToKey(sibling.url)] || 1;
+    //   console.log(entryWeight);
+    //   //map to URL/index mapping
+    //   //If self, skip, this is special condition
+    //   //Compare and swap places with a higher index bookmark that's got fewer hits after the change.
+    // });
+
+    //Re-Order self and siblings in folder
+  });
+};
 
 chrome.webNavigation.onCompleted.addListener(onNavigationCompleted);
