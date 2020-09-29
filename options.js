6adafc2c062e41c3c2e539bfd69ddae9be8730c3
Hudson Akridge <Hudson.Akridge@coxautoinc.com>
2020-09-28 19:38:51 -0500

    Adds a button to reset extension, and a polyfill library for moving to xplatform capabilities

--- options.js
+++ options.js
@@ -1,33 +1,43 @@
 /* eslint-disable no-undef */
 // Saves options to chrome.storage
-function save_options() {
-    let storageType = document.getElementById('storageType').value;
-    let orderType = document.getElementById('orderType').value;
-    chrome.storage.sync.set({
-      storageType: storageType,
-      orderType: orderType
-    }, function() {
-      // Update status to let user know options were saved.
-      let status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(function() {
-        status.textContent = '';
-      }, 750);
-    });
-  }
-  
-  // Restores select box and checkbox state using the preferences
-  // stored in chrome.storage.
-  function restore_options() {
-    // Use default value color = 'red' and likesColor = true.
-    chrome.storage.sync.get({
-      storageType: 'sync',
-      orderType: 'frequency'
-    }, function(items) {
-      document.getElementById('storageType').value = items.storageType;
-      document.getElementById('orderType').value = items.orderType;
-    });
-  }
-  document.addEventListener('DOMContentLoaded', restore_options);
-  document.getElementById('save').addEventListener('click',
-      save_options);
+async function save_options() {
+  let storageType = document.getElementById("storageType").value;
+  let orderType = document.getElementById("orderType").value;
+  await browser.storage.sync.set({
+    storageType: storageType,
+    orderType: orderType,
+  });
+
+  displayMessage("Options saved.");
+}
+
+function displayMessage(message) {
+  // Update status to let user know options were saved.
+  let status = document.getElementById("status");
+  status.textContent = message;
+  setTimeout(function () {
+    status.textContent = "";
+  }, 2000);
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+async function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  let items = await browser.storage.sync.get({
+    storageType: "sync",
+    orderType: "frequency",
+  });
+
+  document.getElementById("storageType").value = items.storageType;
+  document.getElementById("orderType").value = items.orderType;
+}
+
+async function clear_storage() {
+  await browser.storage.sync.clear();
+  await browser.storage.local.clear();
+}
+
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("save").addEventListener("click", save_options);
+document.getElementById("clearStorage").addEventListener("click", clear_storage);
